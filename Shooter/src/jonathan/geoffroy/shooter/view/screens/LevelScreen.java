95ec8dd4a26caceae8d5921f7ce587ba831848a5
Jonathan <geoffroy.jonathan@gmail.com>
2013-08-08 10:18:06 +0000

    add sounds on Level mode

--- Shooter/src/jonathan/geoffroy/shooter/view/screens/LevelScreen.java
+++ Shooter/src/jonathan/geoffroy/shooter/view/screens/LevelScreen.java
@@ -4,19 +4,24 @@ import java.util.ArrayList;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetDescriptor;
+import com.badlogic.gdx.audio.Music;
 
+import jonathan.geoffroy.shooter.Shooter;
 import jonathan.geoffroy.shooter.model.Map;
 import jonathan.geoffroy.shooter.view.actors.MapActor;
+import jonathan.geoffroy.shooter.view.utils.App;
 import jonathan.geoffroy.shooter.view.utils.StageScreen;
 
 public class LevelScreen extends StageScreen {
+	private static final String MUSIC = Shooter.SOUNDS + "Level/music.mp3";
+
 	private int level;
 	private Map map;
 	private MapActor mapActor;
-	
+
 	public LevelScreen() {
 		super();
-		
+
 		level = 1;
 		try {
 			map = Map.load(level);
@@ -24,9 +29,12 @@ public class LevelScreen extends StageScreen {
 			e.printStackTrace();
 		}
 	}
+
+	@SuppressWarnings({ "rawtypes", "unchecked" })
 	@Override
 	public ArrayList<AssetDescriptor<Object>> getAssetDescriptors() {
 		ArrayList<AssetDescriptor<Object>> result = new ArrayList<AssetDescriptor<Object>>();
+		result.add(new AssetDescriptor(MUSIC, Music.class));
 		result.addAll(MapActor.getAssetDescriptors());
 		return result;
 	}
@@ -36,5 +44,8 @@ public class LevelScreen extends StageScreen {
 		mapActor = new MapActor(map);
 		mapActor.setBounds(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		stage.addActor(mapActor);
+
+		Music music = (Music) App.getAsset(MUSIC);
+		music.play();
 	}
 }
