1903a5e2471bb5a58f189944a130aa7f1391d2da
Jonathan <geoffroy.jonathan@gmail.com>
2013-08-09 16:48:11 +0000

    display a joystick on the screen

--- Shooter/src/jonathan/geoffroy/shooter/view/screens/GameScreen.java
+++ Shooter/src/jonathan/geoffroy/shooter/view/screens/GameScreen.java
@@ -6,10 +6,13 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetDescriptor;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 
 import jonathan.geoffroy.shooter.Shooter;
 import jonathan.geoffroy.shooter.model.Map;
 import jonathan.geoffroy.shooter.view.actors.CharacterActor;
+import jonathan.geoffroy.shooter.view.actors.JoystickActor;
 import jonathan.geoffroy.shooter.view.actors.MapActor;
 import jonathan.geoffroy.shooter.view.utils.App;
 import jonathan.geoffroy.shooter.view.utils.StageScreen;
@@ -20,6 +23,7 @@ public abstract class GameScreen extends StageScreen {
 
 	protected MapActor mapActor;
 	protected CharacterActor characterActor;
+	protected JoystickActor joystick;
 	protected Map map;
 
 	@SuppressWarnings({ "rawtypes", "unchecked" })
@@ -29,6 +33,7 @@ public abstract class GameScreen extends StageScreen {
 		result.add(new AssetDescriptor(MUSIC, Music.class));
 		result.addAll(MapActor.getAssetDescriptors());
 		result.addAll(CharacterActor.getAssetDescriptors());
+		result.addAll(JoystickActor.getAssetDescriptors());
 		return result;
 	}
 
@@ -45,6 +50,24 @@ public abstract class GameScreen extends StageScreen {
 				mapActor.getTerrainWidth(), mapActor.getTerrainWidth());
 		stage.addActor(characterActor);
 
+		JoystickActor.initialize();
+		joystick = new JoystickActor();
+		float joystickSize = Math.max(Gdx.graphics.getWidth(), Gdx.graphics.getWidth()) / 10;
+		joystick.setBounds(0, 0, joystickSize, joystickSize);
+		joystick.addListener(new ChangeListener() {
+			@Override
+			public void changed(ChangeEvent event, Actor actor) {
+				if(joystick.getKnobPercentX() >= 0) {
+					characterActor.move(CharacterActor.MOVE_RIGHT);
+				}
+				else {
+					characterActor.move(CharacterActor.MOVE_LEFT);
+				}
+			}
+		});
+		stage.addActor(joystick);
+
+		Gdx.input.setInputProcessor(stage);
 		Music music = (Music) App.getAsset(MUSIC);
 		music.play();
 	}
